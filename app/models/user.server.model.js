dfee6ee7a23435bd9f4e73ad29508db4c8f8fa8c
mndewitt <mdewitt07@gmail.com>
2015-01-03 17:39:17 -0500

    add defaults and modifiers to mongoose User schema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,8 +5,39 @@ var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
 	email: String,
-	username: String,
-	password: String
+	username: {
+		type: String,
+
+		//This is a built in mongoose modifier...
+
+		trim: true
+	},
+
+	website: {
+		type: String,
+
+		//And this is an example of a custom modifier...
+		//Will add http:// to the site if not already added...
+
+		set: function(url) {
+			if(!url) {
+				return url;
+			} else {
+				if(url.indexOf('http://') !==0 && url.indexOf('https://') !==0 ) {
+					url = 'http://' + url;
+				}
+
+				return url;
+			}
+		}
+
+	},
+
+	password: String,
+	created: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 mongoose.model('User', UserSchema);
