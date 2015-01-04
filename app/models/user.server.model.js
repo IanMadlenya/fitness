f642af6595832271dd6cf9b20139d6e26fee4193
mndewitt <mdewitt07@gmail.com>
2015-01-03 22:19:42 -0500

    add built in and custom validators to the Users model

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,13 +4,33 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
 	firstName: String,
 	lastName: String,
-	email: String,
+	email: {
+		type: String,
+		index: true,
+
+		//Example of validation-- must meet regex expression to be saved
+
+		match: /.+\@.+\..+/
+	},
+
 	username: {
 		type: String,
 
 		//This is a built in mongoose modifier...
 
-		trim: true
+		trim: true,
+
+		/*And this is an index.  It tells mongoose that 
+		the entry must be unique.  This helps build
+		efficient queries.  It is common to not allow
+		multiple instances of the same username, as we are 
+		doing here...*/
+
+		unique: true,
+
+		//Validate the field
+
+		required: true
 	},
 
 	website: {
@@ -19,7 +39,13 @@ var UserSchema = new Schema({
 		//And this is an example of a custom modifier...
 		//Will add http:// to the site if not already added...
 
-		set: function(url) {
+		/*Called a 'getter' modifier (as opposed to a 'setter'), because 
+		the modifier will only be applied to previous documents
+		at the time of query, as opposed to all at once (as 'setter' does).
+		This is presumably useful when adding a modifier to a
+		very large database, and performance is a big consideration*/
+
+		get: function(url) {
 			if(!url) {
 				return url;
 			} else {
@@ -33,11 +59,37 @@ var UserSchema = new Schema({
 
 	},
 
-	password: String,
+	password: {
+		type:String,
+
+		//example of a custom validation function
+
+		validate: [
+			function(password) {
+				return password.length >= 6;
+			},
+
+			'Password should be at least 6 characters'
+		]
+	},
+
 	created: {
 		type: Date,
 		default: Date.now
 	}
 });
+
+/*This is an example of a 'virtual' attribute,
+which can take multiple attributes from the Schema
+and create a new attribute out of that, but they are
+not really presented in the document...*/
+
+UserSchema.virtual('fullName').get(function() {
+	return this.firstName + ' ' + this.lastName;
+});
+
+//Make sure to enable 'getter' modifiers and virtual attributes in this next line
+
+UserSchema.set('toJSON', {getters: true, virtuals: true});
 
 mongoose.model('User', UserSchema);
