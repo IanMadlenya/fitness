bd8b3a6b1ffc8224cf8c6b20d099bb48f6f17bc4
mndewitt <mdewitt07@gmail.com>
2015-01-04 20:31:19 -0500

    add first angular view

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, []);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['journal']);
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainApplicationModuleName]);
