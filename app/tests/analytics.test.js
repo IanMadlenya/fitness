aec61a3f8de73409da0eebf6ff5a3d4f3704826e
mndewitt <mdewitt07@gmail.com>
2015-12-28 23:30:29 -0500

    refine test

--- app/tests/analytics.test.js
+++ app/tests/analytics.test.js
@@ -1,16 +1,15 @@
 var assert = require('assert'),
 	mockExerciseData = require('./mocks/exercises'),
-	mongoose = require('mongoose'),
-	moment = require('moment');
+	mongoose = require('mongoose');
 
-//Mock the Exercise mongoos model
+//Mock the Exercise mongoose model
 mongoose.model('Exercise', new mongoose.Schema());
 
 var Exercise = mongoose.model('Exercise'),
 	analyticsController = require('../controllers/analytics.server.controller');
 
-describe('getAverageDaysBetweenWorkouts', function() {
-	it('should return the average number of days', function() {
-		assert.deepEqual(analyticsController.getAverageDaysBetweenWorkouts(mockExerciseData), 1);
+describe('getAverageDaysBetweenWorkouts()', function() {
+	it('should return the average number of days between each exercise', function() {
+		assert.deepEqual(analyticsController.getAverageDaysBetweenWorkouts(mockExerciseData), 3);
 	});
 });
--- app/tests/mocks/exercises.js
+++ app/tests/mocks/exercises.js
@@ -1,7 +1,7 @@
 module.exports = [
     {
-        "_id": " ",
-        "creator": " ",
+        "_id": "",
+        "creator": "",
         "weight": "123",
         "reps": "123",
         "sets": "123",
@@ -10,25 +10,23 @@ module.exports = [
         "created": "2015-12-13T20:11:04.689Z"
     },
     {
-        "_id": "566dd1cdd3446a9a2a5c89c3",
-        "creator": "54e20eb3e0ab4a2e19d9881f",
-        "__v": 0,
+        "_id": "",
+        "creator": "",
         "weight": "123",
         "reps": "123",
         "sets": "123",
         "exerciseSlug": "bench-press",
         "exercise": "bench press",
-        "created": "2015-12-14T20:15:09.355Z"
+        "created": "2015-12-16T20:15:09.355Z"
     },
     {
-        "_id": "566dd1e9d3446a9a2a5c89c4",
-        "creator": "54e20eb3e0ab4a2e19d9881f",
-        "__v": 0,
+        "_id": "",
+        "creator": "",
         "weight": "123",
         "reps": "123",
         "sets": "123",
         "exerciseSlug": "lunge",
         "exercise": "lunge",
-        "created": "2015-12-15T20:15:37.472Z"
+        "created": "2015-12-19T20:15:37.472Z"
     }
 ]
